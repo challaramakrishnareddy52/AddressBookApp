eba32ceac228736fa4efc742f1a863253c8f240f
challaramakrishnareddy52 <challaramakrishnareddy52@gmail.com>
2022-11-22 17:07:14 +0530

    Deleting Contact List

--- AddressBookApp/Program.cs
+++ AddressBookApp/Program.cs
@@ -6,27 +6,36 @@ namespace AddressBookApp
     {
         static void Main(string[] args)
         {
-            ContactList contact = new ContactList();
+            ContactList2 contact = new ContactList2();
             contact.WelcomeMessage();
-        ReEnteringApp:
+        ReEnteringApp:                         
             Console.WriteLine("Select any one from below options \n" +
                 "Press 1 : to Create/Add a new contact\n" +
                 "Press 2 : to Edit/Modify any existing contact\n" +
-                "Press 3 : to view all the contacts in Address Book");
+                "Press 3 : to view all the contacts in Address book.\n" +
+                "Press 4 : to delete any existing contact");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
-                    contact.AddPerson();
+                    contact.AddPerson();        
                     break;
                 case 2:
-                    contact.ListPeople();
+                    contact.EditPerson();       
+                    break;
+                case 3:
+                    contact.ListPeople();       
+                    break;
+                case 4:
+                    contact.RemovePerson();
                     break;
                 default:
                     Console.WriteLine("Invalid Option selected , Please try again ");
                     break;
             }
-            goto ReEnteringApp;
+            goto ReEnteringApp;    
         }
     }
 }
+        
+        
