b536a40b3b42bd2d521e5c63d446232425cde1a9
challaramakrishnareddy52 <challaramakrishnareddy52@gmail.com>
2022-11-22 15:34:40 +0530

    Welcome To Address Book App

---
+++ AddressBookApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AddressBookApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address Book App");
+        }
+    }
+}
