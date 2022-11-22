abc98d273d09e7228b2f377ff96bd5df3e175462
challaramakrishnareddy52 <challaramakrishnareddy52@gmail.com>
2022-11-22 16:05:28 +0530

    Creating Contact

--- AddressBookApp/Program.cs
+++ AddressBookApp/Program.cs
@@ -2,9 +2,28 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Msg()
         {
             Console.WriteLine("Welcome To Address Book App");
+        }
+        public static void CreatingContact()
+        {
+            Console.WriteLine("Enter Your First Name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter Your Last Name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter Your Address");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter Your Phone Number");
+            double phoneNo = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Your Email-Id");
+            string emailId = Console.ReadLine();
+            Console.WriteLine($"\n First Name : {firstName}\n Last Name : {lastName}\n Address : {address}\n Phone Number : {phoneNo}\n Email Id : {emailId}");
+        }
+        static void Main(string[] args)
+        {
+            Msg();
+            CreatingContact();
         }
     }
 }
