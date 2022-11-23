e5400bc6489f019070a747e10ca2a1a332c391d6
challaramakrishnareddy52 <challaramakrishnareddy52@gmail.com>
2022-11-23 21:37:09 +0530

    adding multiple contacts

--- AddressBookApp/Person.cs
+++ AddressBookApp/Person.cs
@@ -1,18 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AddressBookApp
 {
-        public class Person
+    [DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+    public class Person
         {
             public string firstName { get; set; }
             public string lastName { get; set; }
             public string address { get; set; }
             public double phoneNo { get; set; }
             public string emailId { get; set; }
+
+        private string GetDebuggerDisplay()
+        {
+            return ToString();
         }
+    }
     }
 
--- AddressBookApp/Program.cs
+++ AddressBookApp/Program.cs
@@ -1,41 +1,37 @@
-﻿using System;
+﻿using AddressBookApp;
+using System;
 
-namespace AddressBookApp
+internal class Program
 {
-    internal class AddressBook
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
+        ContactList contact = new ContactList();
+        contact.WelcomeMessage();
+    ReEnteringApp:
+        Console.WriteLine("Select any one from below options \n" +
+            "Press 1 : to Create/Add a new contact\n" +
+            "Press 2 : to Edit/Modify any existing contact\n" +
+            "Press 3 : to view all the contacts in Address book.\n" +
+            "Press 4 : to delete any existing contact");
+        int option = Convert.ToInt32(Console.ReadLine());
+        switch (option)
         {
-            ContactList2 contact = new ContactList2();
-            contact.WelcomeMessage();
-        ReEnteringApp:                         
-            Console.WriteLine("Select any one from below options \n" +
-                "Press 1 : to Create/Add a new contact\n" +
-                "Press 2 : to Edit/Modify any existing contact\n" +
-                "Press 3 : to view all the contacts in Address book.\n" +
-                "Press 4 : to delete any existing contact");
-            int option = Convert.ToInt32(Console.ReadLine());
-            switch (option)
-            {
-                case 1:
-                    contact.AddPerson();        
-                    break;
-                case 2:
-                    contact.EditPerson();       
-                    break;
-                case 3:
-                    contact.ListPeople();       
-                    break;
-                case 4:
-                    contact.RemovePerson();
-                    break;
-                default:
-                    Console.WriteLine("Invalid Option selected , Please try again ");
-                    break;
-            }
-            goto ReEnteringApp;    
+            case 1:
+                contact.AddPerson();
+                break;
+            case 2:
+                contact.EditPerson();
+                break;
+            case 3:
+                contact.ListPeople();
+                break;
+            case 4:
+                contact.RemovePerson();
+                break;
+            default:
+                Console.WriteLine("Invalid Option selected , Please try again ");
+                break;
         }
+        goto ReEnteringApp;
     }
 }
-        
-        
